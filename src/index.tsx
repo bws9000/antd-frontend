96e29b835c670dbb2a3f5323110a53908d551976
bws9000 <snyder.burt@gmail.com>
2022-05-12 10:37:33 -0400

    add light/dark mode less/css

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// import { ThemeSwitcherProvider } from "react-css-theme-switcher";
+
+// const themes = {
+//   dark: `/css/dark.theme.css`,
+//   light: `/css/light.theme.css`,
+// };
+
+import './style/theme/dark.theme.less';
+
+import { Button, DatePicker } from 'antd';
+
 const root = ReactDOM.createRoot(
   document.getElementById('app-root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <h1>webpack</h1>
+    {/* <ThemeSwitcherProvider themeMap={themes} defaultTheme="light"> */}
+    <Button type="primary">PRESS ME</Button>
+    <DatePicker placeholder="select date" />
+    {/* </ThemeSwitcherProvider> */}
   </React.StrictMode>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, "src", "index.tsx"),
+  devtool: 'inline-source-map',
   output: {
     path:path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
+      {
+        test: /\.less$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'less-loader',
+            options: {
+              lessOptions: { 
+                // modifyVars: {
+                //   'primary-color': '#0A66C2',
+                //   'link-color': '#0A66C2',
+                //   'border-radius-base': '2px',
+                // },
+                javascriptEnabled: true,
+              },
+            }, 
+          }
+        ]
+      },
       {
         test: /\.?js$/,
         exclude: /node_modules/,
